e85667d41582027cd2d7cbf0fabd085b581c4ff6
dogugnr <d.gunr@yaani.com>
2023-11-05 12:57:04 -0800

    first commint

--- histogram/main.py
+++ histogram/main.py
@@ -1,31 +1,31 @@
 import cv2
+import matplotlib.pyplot as plt
 
-# Resim adı
+
+def show_histogram(image_path, num_bins=256):
+    # Görüntüyü oku
+    img = cv2.imread(image_path)
+
+    # Görüntüyü gri tonlamaya dönüştür
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Görüntü boyutlarını al
+    size_y = img_gray.shape[0]
+    size_x = img_gray.shape[1]
+
+    # Görüntüyü düzleştir
+    flattened = img_gray.reshape([size_x * size_y])
+
+    # Histogram oluştur ve göster
+    plt.hist(flattened, bins=num_bins)
+    plt.title(f"{num_bins}-Bin Histogram")
+    plt.xlabel("Pixel Değerleri")
+    plt.ylabel("Pixel Sayısı")
+    plt.show()
+
+
+# Resim adı ve isteğe bağlı olarak bin sayısını belirleyerek fonksiyonu çağırma işlemi.
 image_name = "/Users/dogugnr/Documents/Yazılım/PythonPyCharm/A.jpg"
-# Görüntüyü oku
-img = cv2.imread(image_name)
-
-# Görüntüyü gri tonlamaya dönüştür
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# Kanalları düzleştir
-# Kanalların histogram ve normalleştirilmiş histogramını elde etmek için hist işlevini kullanın
-size_y = img.shape[0]  # şekli al
-size_x = img.shape[1]
-
-flattened = img.reshape([size_x * size_y])  # Matrisleri düzleştir
-
-# pylot'u içe aktar
-from matplotlib import pyplot as plt
-
-# 256 binli histogram oluştur ve göster
-rhist, _, _ = plt.hist(flattened, bins=256)  #, log=True)
-plt.show()
-
-# 32 binli histogram oluştur ve göster
-rhist, _, _ = plt.hist(flattened, bins=32)  #, log=True)
-plt.show()
-
-# 8 binli histogram oluştur ve göster
-rhist, _, _ = plt.hist(flattened, bins=8)  #, log=True)
-plt.show()
+show_histogram(image_name, num_bins=256)
+show_histogram(image_name, num_bins=32)
+show_histogram(image_name, num_bins=8)
