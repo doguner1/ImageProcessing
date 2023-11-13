d1f169ea770e3ebf421a79852be76f7cb588a31f
dogugnr <d.gunr@yaani.com>
2023-11-13 12:43:40 -0800

    first commint

--- ObjectDetectionOdev2/main.py
+++ ObjectDetectionOdev2/main.py
@@ -1,39 +1,51 @@
 import cv2
 import numpy as np
 
-def detect_red_objects(frame):
-    # Görüntüyü HSV renk uzayına dönüştür
-    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+# Kamerayı başlat
+kamera = cv2.VideoCapture(0)
 
-    # Kırmızı renk için alt ve üst HSV sınırları
-    lower_red = np.array([0, 100, 100])
-    upper_red = np.array([10, 255, 255])
+"""
+Telefondan kamera kullanmak için
 
-    # Kırmızı renk aralığını maskeleme
-    red_mask = cv2.inRange(hsv_frame, lower_red, upper_red)
+# DroidCam uygulaması olabilir..
+ip_address = "192.168...."
+port = 4747
 
-    # Görüntüdeki kırmızı renkli nesneleri belirleme
-    red_objects = cv2.bitwise_and(frame, frame, mask=red_mask)
-
-    return red_objects
-
-# Kamera bağlantısını başlatma
-cap = cv2.VideoCapture(0)
+#Görüntü Çağırma kodu şöyle değişir:
+cv2.VideoCapture(f"http://{ip_address}:{port}/video")
+"""
 
 while True:
-    # Kameradan bir kare al
-    ret, frame = cap.read()
+    # Kameradan görüntü al
+    ret, kare = kamera.read()
+    cv2.imshow('Orjinal Kare',kare)
 
-    # Kırmızı nesneleri tespit et
-    red_objects = detect_red_objects(frame)
+    # Görüntüyü BGR'den HSV'ye dönüştür
+    hsv = cv2.cvtColor(kare, cv2.COLOR_BGR2HSV)
 
-    # Kırmızı nesneleri göster
-    cv2.imshow("Red Objects", red_objects)
+    # Yeşil renk
+    lower_green = np.array([36, 25, 25])
+    upper_green = np.array([70, 255, 255])
+    mask1 = cv2.inRange(hsv, lower_green, upper_green)
 
-    # Q tuşuna basıldığında döngüyü sonlandır
+    # Mavi renk
+    lower_blue = np.array([94, 80, 2])
+    upper_blue = np.array([120, 255, 255])
+    mask2 = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    # Yeşil ve mavi renk alanlarını birleştir
+    mask = mask1 + mask2
+
+    # Maskı burada siyah ve beyaz yapıyoruz
+    kare[mask > 0] = [0, 0, 0]
+
+    # Görüntüyü göster
+    cv2.imshow('Renk Tespiti', kare)
+
+    # Çıkış yapmak için 'q' tuşuna basın
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Kamera bağlantısını kapat
-cap.release()
+# Kamerayı serbest bırak ve pencereleri kapat
+kamera.release()
 cv2.destroyAllWindows()
